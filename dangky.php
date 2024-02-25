a1169c1447476a49d15d9518e5f0faffb703f4ac
TuanBuonSleep <156689488+tuandeptrai1@users.noreply.github.com>
2024-02-25 21:27:07 +0700

    Update dangky.php

--- dangky.php
+++ dangky.php
@@ -10,6 +10,7 @@ function updateInfo() {
         var selectNhom = document.getElementById('selectNhom');
         var nguoiHuongDanInput = document.getElementById('NguoiHuongDan');
         var moTaInput = document.getElementById('MoTa');
+        var SoLuongsvInput = document.getElementById('SoLuongsv');
         
         
         var selectedOption = selectNhom.options[selectNhom.selectedIndex];
@@ -17,6 +18,7 @@ function updateInfo() {
      
         nguoiHuongDanInput.value = selectedOption.getAttribute('data-nguoihuongdan');
         moTaInput.value = selectedOption.getAttribute('data-mota');
+        SoLuongsvInput.value = selectedOption.getAttribute('data-soluongsv');
     }
 </script>
 </head>
@@ -33,7 +35,7 @@ Nhóm:
         $sql = "SELECT * FROM detai";
         $result = mysqli_query($conn, $sql);
         foreach ($result as $row) {
-            echo '<option data-nguoihuongdan="' . $row['NguoiHuongDan'] . '" data-mota="' . $row['MoTa'] . '">' . $row['Ten'] . '</option>';
+            echo '<option data-nguoihuongdan="' . $row['NguoiHuongDan'] . '" data-mota="' . $row['MoTa'] . '" data-soluongsv="' . $row['SoLuongsv'] . '">' . $row['Ten'] . '</option>';
         }
     ?>
 </select></br>
@@ -41,6 +43,7 @@ Nhóm:
 Người hướng dẫn: <input type="text" id="NguoiHuongDan" name="NguoiHuongDan" value="" readonly>
 
 Mô Tả: </br> <textarea id="MoTa" name="MoTa" value="" style="width: 298px; height: 130px;" readonly>  </textarea></br>
+Số lượng sinh viên còn lại: <input type="text" id="SoLuongsv" name="SoLuongsv" value="" readonly>
 MSSV_HoTen: <input type="text" name="HoTen" value="" required/>
 
    <h3 style="color: green ">------------------------------------------------</h3>
